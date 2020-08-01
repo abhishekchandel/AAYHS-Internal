61d79a54faac5aa479a2a21c2515179fcd9434da
Deepak <deepaksharmawins@gmail.com>
2020-08-01 12:36:44 +0530

    new changes commit

--- api/AAYHS.Core/DTOs/Response/Common/ClassResponse.cs
+++ api/AAYHS.Core/DTOs/Response/Common/ClassResponse.cs
@@ -28,6 +28,10 @@ namespace AAYHS.Core.DTOs.Response.Common
     {
         public List<string> ClassExhibitorHorse { get; set; }
     }
+    public class ClassExhibitorHorses
+    {
+        public List<string> ClassExhibitorHorse { get; set; }
+    }
     public class GetClass
     {
         public int ClassId { get; set; } 
