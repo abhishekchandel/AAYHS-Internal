33643dda2ba0ca457bc31c841a8d84d0ff075e7f
Abhishek <harmeshwins@gmail.com>
2020-09-07 09:29:52 +0530

    Commiting changes

--- angular/aayhs/src/app/shared/ui/modals/sponsor-info-modal/sponsor-info-modal.component.ts
+++ angular/aayhs/src/app/shared/ui/modals/sponsor-info-modal/sponsor-info-modal.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatSnackbarComponent } from '../../mat-snackbar/mat-snackbar.component'
 
 @Component({
   selector: 'app-sponsor-info-modal',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sponsor-info-modal.component.scss']
 })
 export class SponsorInfoModalComponent implements OnInit {
-
-  constructor() { }
+sponsorDetails:{
+  sponsorName:null;
+  contactName:null;
+  phone:null;
+  email:null;
+  address:null;
+  amount:null;
+  state:null;
+  city:null;
+  zipcode:null;
+  sponsorId:null
+}
+  constructor(public dialogRef: MatDialogRef<SponsorInfoModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private snackBar: MatSnackbarComponent) { }
 
   ngOnInit(): void {
+    debugger;
+    this.sponsorDetails=this.data;
+  }
+
+  onDismiss(): void {
+    // Close the dialog, return false
+    this.dialogRef.close(false);
   }
 
 }
