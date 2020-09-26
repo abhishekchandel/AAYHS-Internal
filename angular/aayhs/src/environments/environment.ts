0d0b7218893389f3dd70c95d8b40978eea3abfe1
Abhishek <abhishekchandelwins@gmail.com>
2020-09-26 10:49:45 +0530

    Added config

--- angular/aayhs/src/environments/environment.ts
+++ angular/aayhs/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+   baseApiUrl: "https://localhost:44386/api/",
+   filesUrl:"https://localhost:44386"
 };
 
 /*
