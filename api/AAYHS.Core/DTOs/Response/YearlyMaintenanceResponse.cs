4a7ea594d09c3f99a1ba94961a1e252d5a81b28b
SaddamHusaain <Saddamhusaainwins@gmail.com>
2020-09-29 12:08:44 +0530

    Committing Changes

--- api/AAYHS.Core/DTOs/Response/YearlyMaintenanceResponse.cs
+++ api/AAYHS.Core/DTOs/Response/YearlyMaintenanceResponse.cs
@@ -37,6 +37,8 @@ namespace AAYHS.Core.DTOs.Response
     {
         public int UserId { get; set; }
         public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public string Email { get; set; }
         public bool IsActive { get; set; }
         public bool IsApproved { get; set; }
