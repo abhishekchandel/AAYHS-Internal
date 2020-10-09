8e3c539a3aee27e9464682bdf07622123683985e
Abhishek <abhishekchandelwins@gmail.com>
2020-10-09 10:12:36 +0530

    Added report

---
+++ angular/aayhs/src/app/core/services/report.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ReportService {
+  api =environment.baseApiUrl;
+  constructor(private http: HttpClient) { }
+
+  getProgramSheet(id:number){
+    return this.http.get<any>(`${this.api}Report/GetProgramsReport?classId=${id}`);
+  }
+}
